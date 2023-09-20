ff4efe236032cb736a8efdfb8bc232691f72098e
Grzegorz Witkowski <witkowski1519@gmail.com>
2023-09-20 23:52:48 +0200

    feat: added navbar and user profiles

--- TwitterTwo/TwitterToo/admin.py
+++ TwitterTwo/TwitterToo/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ['username']
+
+
+admin.site.unregister(Group)
+admin.site.unregister(User)
+
+admin.site.register(User, UserAdmin)
+
