2e0ee4ff5ffc1d43d45ec6b0ec47c99b2440ab62
Grzegorz Witkowski <witkowski1519@gmail.com>
2023-09-21 18:04:02 +0200

    feat: added profile page

--- TwitterTwo/TwitterToo/urls.py
+++ TwitterTwo/TwitterToo/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('explore_profiles/', views.explore_profiles, name='explore_profiles')
+    path('explore_profiles/', views.explore_profiles, name='explore_profiles'),
+    path('profile/<int:pk>', views.profile, name='profile'),
 ]
--- TwitterTwo/TwitterToo/views.py
+++ TwitterTwo/TwitterToo/views.py
@@ -5,6 +5,15 @@ from .models import Profile
 def home(request):
     return render(request, 'home.html', {})
 
+
+def profile(request, pk):
+    if request.user.is_authenticated:
+        profile = Profile.objects.get(user_id=pk)
+        return render(request, 'profile.html', {'profile': profile})
+    else:
+        messages.success(request, ('You must be logged in to view this page!'))
+        return redirect('home')
+
 def explore_profiles(request):
     if request.user.is_authenticated:
         profiles = Profile.objects.exclude(user=request.user)
