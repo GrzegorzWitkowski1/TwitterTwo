51c75bd1cb7860e7f1c67a7237aa7cb1a0244d8b
Grzegorz Witkowski <witkowski1519@gmail.com>
2023-09-21 10:10:35 +0200

    feat: self-follow on profile creation

--- TwitterTwo/TwitterToo/models.py
+++ TwitterTwo/TwitterToo/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -12,3 +14,11 @@ class Profile(models.Model):
 
     def __str__(self) -> str:
         return self.user.username
+    
+@receiver(post_save, sender=User)    
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        user_profile.follow.set([instance.profile.id])
+        user_profile.save()
