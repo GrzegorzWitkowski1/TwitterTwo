f3a73606f4a9ded22384d1626031b237c5eee815
Grzegorz Witkowski <witkowski1519@gmail.com>
2023-09-21 22:30:52 +0200

    feat: created functional profile page with follows

--- TwitterTwo/TwitterToo/models.py
+++ TwitterTwo/TwitterToo/models.py
@@ -5,7 +5,7 @@ from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    follow = models.ManyToManyField(
+    follows = models.ManyToManyField(
         'self',
         related_name='followed_by',
         symmetrical=False,
