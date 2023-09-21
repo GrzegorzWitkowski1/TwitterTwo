d558977fcf6fe6eabd38a9e622607ffac7dedb5f
Grzegorz Witkowski <witkowski1519@gmail.com>
2023-09-21 17:45:33 +0200

    feat: created error message if not logged in

--- TwitterTwo/TwitterToo/views.py
+++ TwitterTwo/TwitterToo/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Profile
 
 def home(request):
     return render(request, 'home.html', {})
 
 def explore_profiles(request):
-    profiles = Profile.objects.exclude(user=request.user)
-    return render(request, 'explore_profiles.html', {'profiles': profiles})
+    if request.user.is_authenticated:
+        profiles = Profile.objects.exclude(user=request.user)
+        return render(request, 'explore_profiles.html', {'profiles': profiles})
+    else:
+        messages.success(request, ('You must be logged in to view this page!'))
+        return redirect('home')
