2012c1fbfb8e86adb50970ecbf1341ddab204e4e
Grzegorz Witkowski <witkowski1519@gmail.com>
2023-09-22 21:55:32 +0200

    feat: added working follow and unfollow button

--- TwitterTwo/TwitterToo/views.py
+++ TwitterTwo/TwitterToo/views.py
@@ -9,6 +9,14 @@ def home(request):
 def profile(request, pk):
     if request.user.is_authenticated:
         profile = Profile.objects.get(user_id=pk)
+        if request.method == 'POST':
+            current_user_profile = request.user.profile
+            action = request.POST['follow']
+            if action == 'unfollow':
+                current_user_profile.follows.remove(profile)
+            elif action == 'follow':
+                current_user_profile.follows.add(profile)
+            current_user_profile.save()
         return render(request, 'profile.html', {'profile': profile})
     else:
         messages.success(request, ('You must be logged in to view this page!'))
