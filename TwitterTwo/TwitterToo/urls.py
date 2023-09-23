3d8d1de35c0e89a185603003df98675cda82b0b4
Grzegorz Witkowski <witkowski1519@gmail.com>
2023-09-23 13:34:51 +0200

    feat: added login and logut

--- TwitterTwo/TwitterToo/urls.py
+++ TwitterTwo/TwitterToo/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('explore_profiles/', views.explore_profiles, name='explore_profiles'),
     path('profile/<int:pk>', views.profile, name='profile'),
+    path('login/', views.login_user, name='login'),
+    path('logout', views.logout_user, name='logout')
 ]
--- TwitterTwo/TwitterToo/views.py
+++ TwitterTwo/TwitterToo/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Profile, Tweet
 from .forms import TweetForm
+from django.contrib.auth import authenticate, login, logout
+
 
 def home(request):
     if request.user.is_authenticated:
@@ -55,3 +57,28 @@ def explore_profiles(request):
     else:
         messages.success(request, ('You must be logged in to view this page!'))
         return redirect('home')
+    
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, ('You have been logged in!'))
+            return redirect('home')
+        
+        else:
+            messages.success(request, ('There was an error logging in!'))
+            return redirect('login')
+
+    else:
+        return render(request, 'login.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ('You have been logged out!'))
+    return redirect('home')
